fc1d981fe22abbfcc3f0be285519f479949a3995
wrsorensen <wrsorensen@hotmail.com>
2026-08-16 21:51:47 -0500

    v0.5.19 - ESPN asset cache bust

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*
   2B1C FFL
-  v0.5.18 - clean hero actions
+  v0.5.19 - ESPN asset cache bust
 */
 const APPS_SCRIPT_API_URL = "https://script.google.com/macros/s/AKfycbx1r1DRzTOZj9wy1NRspGRc-Nq51oypZGl6upojMG4NUGmZMH7GMCPPWBClFRl08rAtaA/exec";
 const APP_DATA_CACHE_KEY = "2b1cAppDataCacheV1";
